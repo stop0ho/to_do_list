571f5e0f7ec196011a08653747bdf97c7b9fc96a
죠 <stop.ho@khu.ac.kr>
2023-08-28 21:04:40 +0900

    Feat: 체크박스 누르면 취소선 생기는 기능
    - 항목 여러개면 적용 안 되는 issue 수정 필요

--- todo.js
+++ todo.js
@@ -1,32 +1,77 @@
-function addTodo(event) {
+function makeDiv() {
   const div = document.createElement('div');
+  div.className = 'todo-list-container';
+  return div;
+}
+
+function makeSpan() {
   const span = document.createElement('span');
-  const input = document.createElement('input');
-  const button = document.createElement('button');
-
-  input.id = 'checkBox';
-  input.className = 'check-box';
-  input.setAttribute('type', 'checkbox');
-  console.log(input);
-  div.appendChild(input);
-
   span.innerText = addValue.value;
   addValue.value = '';
   span.className = 'list-content';
-  span.id = 'list-content';
-  div.appendChild(span);
+  span.id = 'listContent';
+  return span;
+}
 
+function makeCheckBox() {
+  const input = document.createElement('input');
+  input.id = 'checkBox';
+  input.className = 'check-box';
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('onClick', 'isChecked()');
+  return input;
+}
+
+function makeDeleteButton() {
+  button = document.createElement('button');
   button.innerText = 'X';
   button.setAttribute('type', 'button');
   button.setAttribute('onClick', 'deleteItem()');
-  button.className = 'list-btn';
-  button.id = 'list-btn';
-  div.appendChild(button);
+  button.className = 'list-delete-btn';
+  button.id = 'list-delete-btn';
+  return button;
+}
 
-  div.className = 'list';
+function makeEditButton() {
+  button = document.createElement('button');
+  button.innerText = '편집';
+  button.setAttribute('type', 'button');
+  button.setAttribute('onClick', 'editItem()');
+  button.className = 'list-edit-btn';
+  button.id = 'list-edit-btn';
+  return button;
+}
+
+function isChecked() {
+  const isCheck = checkBox.checked;
+  if (isCheck == true) {
+    listContent.className = 'checked-list-content';
+  } else {
+    listContent.className = 'list-content';
+  }
+}
+
+function addTodo(event) {
+  const div = makeDiv();
+  const span = makeSpan();
+  const input = makeCheckBox();
+  const deleteBtn = makeDeleteButton();
+  const editBtn = makeEditButton();
+
+  div.appendChild(input);
+  div.appendChild(span);
+  div.appendChild(editBtn);
+  div.appendChild(deleteBtn);
+
   console.log(div);
   console.log(span);
   toDO.appendChild(div);
 }
 
-function deleteItem(event) {}
+function deleteItem(event) {
+  console.log('delete');
+}
+
+function editItem(event) {
+  console.log('edit');
+}
